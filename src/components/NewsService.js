61e246a774243590e43e7f078995b392aab48391
theredmac <102948598+theredmac@users.noreply.github.com>
2023-10-16 11:49:32 +0530

    Update NewsService.js

--- src/components/NewsService.js
+++ src/components/NewsService.js
@@ -5,9 +5,21 @@ import axios from 'axios';
 const API_KEY = '4866663f0fd140e48cf56b86daf0bd98'; // Replace with your News API key
 const API_URL = 'https://newsapi.org/v2/top-headlines';
 
-const fetchNews = async () => {
+const fetchNews = async (country = 'us', category = '') => {
   try {
-    const response = await axios.get(`${API_URL}?country=us&apiKey=${API_KEY}`);
+    const params = {
+      apiKey: API_KEY,
+      country: country,
+    };
+
+    if (category) {
+      params.category = category;
+    }
+
+    const response = await axios.get(API_URL, {
+      params: params,
+    });
+
     return response.data.articles;
   } catch (error) {
     throw error;
