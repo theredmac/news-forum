a54c0338421eed4a72c2eab8b7266b79a8bf6977
theredmac <102948598+theredmac@users.noreply.github.com>
2023-10-16 11:50:47 +0530

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
-function SearchBar({ searchTerm, onSearch, setSearchTerm }) {
+function SearchBar({
+  searchTerm,
+  onSearch,
+  setSearchTerm,
+  onCategoryChange,
+  onCountryChange,
+  selectedCategory,
+  selectedCountry,
+}) {
   const handleInputChange = (event) => {
     setSearchTerm(event.target.value);
   };
@@ -13,6 +21,26 @@ function SearchBar({ searchTerm, onSearch, setSearchTerm }) {
         value={searchTerm}
         onChange={handleInputChange}
       />
+      <select onChange={(e) => onCategoryChange(e.target.value)} value={selectedCategory}>
+        <option value="">Select Category</option>
+        <option value="business">Business</option>
+        <option value="entertainment">Entertainment</option>
+        <option value="general">General</option>
+        <option value="health">Health</option>
+        <option value="science">Science</option>
+        <option value="sports">Sports</option>
+        <option value="technology">Technology</option>
+      </select>
+      <select onChange={(e) => onCountryChange(e.target.value)} value={selectedCountry}>
+        <option value="ca">Canada</option>
+        <option value="in">India</option>
+        <option value="gb">United Kingdom</option>
+        <option value="us">United States</option>
+        
+        
+
+        {/* Add more countries here */}
+      </select>
       <button onClick={onSearch}>Search</button>
     </div>
   );
